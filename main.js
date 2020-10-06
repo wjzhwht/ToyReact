0246f8d6f65899cb4a3847dc92acc504a8a9c73c
wht <wjzhwht@126.com>
2020-10-06 23:25:13 +0800

    Lesson one

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
-for(let i of [1,2,3]){
-    console.log(i);
+import {createElement, Component, render} from "./toy-react.js"
+
+class MyComponent extends Component{
+    render(){
+        return <div>
+            <h1> My Component</h1>
+            {this.children}
+        </div>
+    }
 }
 
-function createElement(tagName, attributes, ...children){
-    let e=document.createElement(tagName);
-    for(let p in attributes){
-        e.setAttribute(p,attributes[p]);
-    }
-    for(let child of children){
-        if(typeof child == "string"){
-            child = document.createTextNode(child);
-        }
-        e.appendChild(child);
-    }
-    return e;
-}
-/*
-window.a=<div id= "a" class="c">
+render(<MyComponent id= "a" class="c">
     <div>abc</div>
     <div></div>
     <div></div>
-</div>
-*/
-
-document.body.appendChild(<div id= "a" class="c">
-    <div>abc</div>
-    <div></div>
-    <div></div>
-</div>);
+</MyComponent>,document.body);
